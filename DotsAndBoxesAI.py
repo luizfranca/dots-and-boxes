dabe0cc5647cb7c917ad786261e971878c003569
Luiz Daniel Ramos Franca <luizdaniel.r.f@gmail.com>
2017-03-04 00:31:23 -0300

    Added depth limit

--- DotsAndBoxesAI.py
+++ DotsAndBoxesAI.py
@@ -10,10 +10,10 @@ def calculate_score(board, player):
 				counter += 1
 	return counter
 
-def alphabeta(node, alpha = Decimal("-Infinity"), beta = Decimal("Infinity"), is_max = True, player = True, move = (0,0)):
+def alphabeta(node, depth = 10, alpha = Decimal("-Infinity"), beta = Decimal("Infinity"), is_max = True, player = True, move = (0,0)):
 	children = node.list_moves()
 
-	if len(children) == 0:
+	if len(children) == 0 or depth == 0:
 		return [move, calculate_score(node, player)]
 
 	if is_max:
@@ -22,7 +22,7 @@ def alphabeta(node, alpha = Decimal("-Infinity"), beta = Decimal("Infinity"), is
 		for x, y in children:
 			current = copy.deepcopy(node)
 			turn = current.move(x, y, player)
-			temp = alphabeta(current, alpha, beta, turn, player, (x, y))
+			temp = alphabeta(current, depth - 1, alpha, beta, turn, player, (x, y))
 			if temp[1] > best_score:
 				best_move = (x, y)
 				best_score = temp[1]
@@ -38,7 +38,7 @@ def alphabeta(node, alpha = Decimal("-Infinity"), beta = Decimal("Infinity"), is
 		for x, y in children:
 			current = copy.deepcopy(node)
 			turn = current.move(x, y, not player)
-			temp = alphabeta(current, alpha, beta, not turn, player, (x, y))
+			temp = alphabeta(current, depth - 1, alpha, beta, not turn, player, (x, y))
 			if temp[1] < worse_score:
 				worse_move = (x, y)
 				worse_score = temp[1]
