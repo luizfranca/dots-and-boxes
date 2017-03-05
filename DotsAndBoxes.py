01310872fafe7b996f4b145890a6ba2dd22cbf5f
Luiz Daniel Ramos Franca <luizdaniel.r.f@gmail.com>
2017-03-04 23:43:39 -0300

    Changed from tabs to spaces

--- DotsAndBoxes.py
+++ DotsAndBoxes.py
@@ -1,130 +1,130 @@
 class Board:
 
-	def __init__(self, n = 0, m = 0):
-		self.board = [[Box() for j in xrange(m - 1)] for i in xrange(n - 1)]
+    def __init__(self, n = 0, m = 0):
+        self.board = [[Box() for j in xrange(m - 1)] for i in xrange(n - 1)]
 
-	def input_board(self, board_string):
-		m = board_string.split("|")[0].count(".")
-		n = board_string.count(".") / m
+    def input_board(self, board_string):
+        m = board_string.split("|")[0].count(".")
+        n = board_string.count(".") / m
 
-		self.board = [[Box() for j in xrange(m - 1)] for i in xrange(n - 1)]
+        self.board = [[Box() for j in xrange(m - 1)] for i in xrange(n - 1)]
 
-		board_list = board_string.upper().split("|")
-		for i in range(len(board_list)):
-			for j in range(len(board_list[i])):
-				if board_list[i][j] == "X":
-					self.move(i, j, None)
+        board_list = board_string.upper().split("|")
+        for i in range(len(board_list)):
+            for j in range(len(board_list[i])):
+                if board_list[i][j] == "X":
+                    self.move(i, j, None)
 
-		for i in range(len(board_list)):
-			for j in range(len(board_list[i])):
-				if board_list[i][j] == "B" or board_list[i][j] == "W":
-					self.board[i / 2][j / 2].player = True if board_list[i][j] == "W" else False
+        for i in range(len(board_list)):
+            for j in range(len(board_list[i])):
+                if board_list[i][j] == "B" or board_list[i][j] == "W":
+                    self.board[i / 2][j / 2].player = True if board_list[i][j] == "W" else False
 
-	def to_string(self):
-		board_string = ""
+    def to_string(self):
+        board_string = ""
 
-		for row in self.board:
-			row1, row2, row3 = "", "", ""
+        for row in self.board:
+            row1, row2, row3 = "", "", ""
 
-			for box in row:
-				box_string = box.to_string().split("|")
-				row1 += box_string[0]
-				row2 += box_string[1]
-				row3 += box_string[2]
+            for box in row:
+                box_string = box.to_string().split("|")
+                row1 += box_string[0]
+                row2 += box_string[1]
+                row3 += box_string[2]
 
-			board_string += row1 + "|" + row2 + "|"
+            board_string += row1 + "|" + row2 + "|"
 
-		board_string += row3
-		return board_string.replace("..", ".").replace("xx", "x").replace("__", "_")
+        board_string += row3
+        return board_string.replace("..", ".").replace("xx", "x").replace("__", "_")
 
-	def move(self, x, y, player):
-		if (x == 0 or y == 0):
-			row = x / 2
-			col = y / 2
-			pos = y == 0
-		elif (x % 2 == 0):
-			row = x / 2 - 1
-			col = y / 2
-			pos = 3
-		else:
-			row = x / 2
-			col = y / 2 - 1
-			pos = 2
+    def move(self, x, y, player):
+        if (x == 0 or y == 0):
+            row = x / 2
+            col = y / 2
+            pos = y == 0
+        elif (x % 2 == 0):
+            row = x / 2 - 1
+            col = y / 2
+            pos = 3
+        else:
+            row = x / 2
+            col = y / 2 - 1
+            pos = 2
 
-		completed = self.board[row][col].move(pos, player)
-		comp = False
+        completed = self.board[row][col].move(pos, player)
+        comp = False
 
-		if (row - 1 >= 0 and pos == 0):
-			comp = self.board[row - 1][col].move(3, player)
-		elif (col - 1 >= 0 and pos == 1):
-			comp = self.board[row][col - 1].move(2, player)
-		elif (col + 1 < len(self.board[row]) and pos == 2):
-			comp = self.board[row][col + 1].move (1, player)
-		elif (row + 1 < len(self.board) and pos == 3):
-			comp = self.board[row + 1][col].move(0, player)
-		
-		return completed or comp
+        if (row - 1 >= 0 and pos == 0):
+            comp = self.board[row - 1][col].move(3, player)
+        elif (col - 1 >= 0 and pos == 1):
+            comp = self.board[row][col - 1].move(2, player)
+        elif (col + 1 < len(self.board[row]) and pos == 2):
+            comp = self.board[row][col + 1].move (1, player)
+        elif (row + 1 < len(self.board) and pos == 3):
+            comp = self.board[row + 1][col].move(0, player)
+        
+        return completed or comp
 
-	def _convert_move_format(self, move): # From my representation to Pablo's
-		x, y = 0, 0
-		if move[2] == 0 or move[2] == 3:
-			x = (move[0] + 1 * (move[2] == 3)) * 2
-			y = (move[1] * 2) + 1
-		else:
-			x = (move[0] * 2) + 1
-			y = (move[1] + 1 * (move[2] == 2)) * 2
+    def _convert_move_format(self, move): # From my representation to Pablo's
+        x, y = 0, 0
+        if move[2] == 0 or move[2] == 3:
+            x = (move[0] + 1 * (move[2] == 3)) * 2
+            y = (move[1] * 2) + 1
+        else:
+            x = (move[0] * 2) + 1
+            y = (move[1] + 1 * (move[2] == 2)) * 2
 
-		return (x, y)
+        return (x, y)
 
-	def list_moves(self):
-		moves = []
-		for i in range(len(self.board)):
-			for j in range(len(self.board[i])):
-				moves += map(lambda x : self._convert_move_format((i, j, x)), self.board[i][j].list_moves())
-		return list(set(moves))
+    def list_moves(self):
+        moves = []
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                moves += map(lambda x : self._convert_move_format((i, j, x)), self.board[i][j].list_moves())
+        return list(set(moves))
 
-	def is_finished(self):
-		for row in self.board:
-			for item in row:
-				if item.player == None:
-					return False
-		return True
+    def is_finished(self):
+        for row in self.board:
+            for item in row:
+                if item.player == None:
+                    return False
+        return True
 
-	def copy(self):
-		board_matrix = []
-		for row in self.board:
-			board_matrix += [[]]
-			for item in row:
-				board_matrix[-1].append(Box(item.edges[:], item.player))
-		board = Board()
-		board.board = board_matrix
-		return board
+    def copy(self):
+        board_matrix = []
+        for row in self.board:
+            board_matrix += [[]]
+            for item in row:
+                board_matrix[-1].append(Box(item.edges[:], item.player))
+        board = Board()
+        board.board = board_matrix
+        return board
 
 class Box:
-	
-	def __init__(self, edges = None, player = None):
-		self.edges = [False, False, False, False] if edges == None else edges # UP LEFT RIGHT DOWN
-		self.player = player  # True represents your box and False the other box
+    
+    def __init__(self, edges = None, player = None):
+        self.edges = [False, False, False, False] if edges == None else edges # UP LEFT RIGHT DOWN
+        self.player = player  # True represents your box and False the other box
 
-	def move(self, n, player):
-		self.edges[n] = True
-		self.player = player if self.is_complete() else None
-		return self.is_complete()
+    def move(self, n, player):
+        self.edges[n] = True
+        self.player = player if self.is_complete() else None
+        return self.is_complete()
 
-	def is_complete(self):
-		return sum(self.edges) == 4
+    def is_complete(self):
+        return sum(self.edges) == 4
 
-	def list_moves(self):
-		moves = []
-		for i in range(4):
-			if not self.edges[i]:
-				moves.append(i)
-		return moves
+    def list_moves(self):
+        moves = []
+        for i in range(4):
+            if not self.edges[i]:
+                moves.append(i)
+        return moves
 
-	def to_string(self): # parameter player represents the main player
-		string = "." + ("x" if self.edges[0] else "_") + ".|" + \
-		("x" if self.edges[1] else "_") + \
-		("*" if self.player == None else ("W" if self.player else "B")) + \
-		("x" if self.edges[2] else "_") + "|." + \
-		("x" if self.edges[3] else "_") + "."
-		return string
+    def to_string(self): # parameter player represents the main player
+        string = "." + ("x" if self.edges[0] else "_") + ".|" + \
+        ("x" if self.edges[1] else "_") + \
+        ("*" if self.player == None else ("W" if self.player else "B")) + \
+        ("x" if self.edges[2] else "_") + "|." + \
+        ("x" if self.edges[3] else "_") + "."
+        return string
--- DotsAndBoxesAI.py
+++ DotsAndBoxesAI.py
@@ -1,52 +1,47 @@
-import copy
 from decimal import Decimal
 from DotsAndBoxes import *
 
 def calculate_score(board, player):
-	counter = 0
-	for row in board.board:
-		for item in row:
-			if item.player == player:
-				counter += 1
-			if item.player == (not player):
-				counter -= 1
-	return counter
+    counter = 0
+    for row in board.board:
+        for item in row:
+            if item.player == player:
+                counter += 1
+            if item.player == (not player):
+                counter -= 1
+    return counter
 
 def alphabeta(node, depth = 10, alpha = Decimal("-Infinity"), beta = Decimal("Infinity"), is_max = True, player = True, move = (0,0)):
-	children = node.list_moves()
-	
-	if len(children) == 0 or depth == 0:
-		return [move, calculate_score(node, player)]
+    children = node.list_moves()
 
-	if is_max:
-		best_move =  ()
-		best_score = Decimal("-Infinity")
-		for x, y in children:
-			current = node.copy()
-			turn = current.move(x, y, player)
-			temp = alphabeta(current, depth - 1, alpha, beta, turn, player, (x, y))
-			if temp[1] > best_score:
-				best_move = (x, y)
-				best_score = temp[1]
+    if len(children) == 0 or depth == 0:
+        return [move, calculate_score(node, player)]
 
-			alpha = max(best_score, alpha)
-			if beta <= alpha:
-				break
-
-		return [best_move, best_score]
-	else:
-		worse_move = ()
-		worse_score = Decimal("Infinity")
-		for x, y in children:
-			current = node.copy()
-			turn = current.move(x, y, not player)
-			temp = alphabeta(current, depth - 1, alpha, beta, not turn, player, (x, y))
-			if temp[1] < worse_score:
-				worse_move = (x, y)
-				worse_score = temp[1]
-
-			beta = min(beta, worse_score)
-			if beta <= alpha:
-				break
-
-		return [worse_move, worse_score]
+    if is_max:
+        best_move =  ()
+        best_score = Decimal("-Infinity")
+        for x, y in children:
+            current = node.copy()
+            turn = current.move(x, y, player)
+            temp = alphabeta(current, depth - 1, alpha, beta, turn, player, (x, y))
+            if temp[1] > best_score:
+                best_move = (x, y)
+                best_score = temp[1]
+            alpha = max(best_score, alpha)
+            if beta <= alpha:
+                break
+        return [best_move, best_score]
+    else:
+        worse_move = ()
+        worse_score = Decimal("Infinity")
+        for x, y in children:
+            current = node.copy()
+            turn = current.move(x, y, not player)
+            temp = alphabeta(current, depth - 1, alpha, beta, not turn, player, (x, y))
+            if temp[1] < worse_score:
+                worse_move = (x, y)
+                worse_score = temp[1]
+            beta = min(beta, worse_score)
+            if beta <= alpha:
+                break
+        return [worse_move, worse_score]
