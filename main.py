dfe8535461837455e4fcc5591c20d795712f0c95
Luiz Daniel Ramos Franca <luizdaniel.r.f@gmail.com>
2017-03-12 14:11:20 -0300

    Added new parameter main.py

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import DotsAndBoxesAI as ai
 
 
 if __name__ == "__main__":
-	if (len(sys.argv) > 2):
-		player = sys.argv[1] == "B"
-		board = sys.argv[2]
+    if (len(sys.argv) > 2):
+        player = sys.argv[1] == "B"
+        board = sys.argv[2]
 
-		game = dab.DotsAndBoxes()
-		game.input_board(board)
-	else:
-		game = dab.DotsAndBoxes(4, 4)
-		player = True
+        game = dab.DotsAndBoxes()
+        game.input_board(board)
+    else:
+        game = dab.DotsAndBoxes(4, 4)
+        player = True
 
-print "%d %d" % ai.alphabeta(game, 8, player = player)[0]
+    if (len(sys.argv) > 3):
+        print "%d %d" % ai.alphabeta(game, int(sys.argv[3]), player = player)[0]
+    else:
+        print "%d %d" % ai.alphabeta(game, 7, player = player)[0]
